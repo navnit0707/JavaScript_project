935e8d6398a62e2dad31b4ab4da61fae5abe8dcc
navnit0707 <knavnit81@gmail.com>
2022-10-15 16:24:56 +0530

    declaring varibale for differnet query selector to save the code from writing multiple time

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
